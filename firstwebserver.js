94d2fa31ad2c2c381d53ce1a0aec73b013f80dd6
Aditya <aditya.pandey.241103@gmail.com>
2023-06-25 22:17:51 +0530

    Routing in node

--- firstwebserver.js
+++ firstwebserver.js
@@ -9,9 +9,21 @@ const html = fs.readFileSync('./Template/index.html', 'utf-8')
 // STEP 1 : CREATE A SERVER 
 
 const server = http.createServer((request, response) => {
-    console.log('A new request has been recieved');
-    response.end(html)
-    console.log(response)
+    // console.log('A new request has been recieved');
+    // response.end(html)
+    // console.log(response)
+    let path = request.url;
+    if (path === '/' || path.toLocaleLowerCase() === '/home') {
+        response.end(html.replace('{{%content%}}', 'You are in Home Page'))
+    }
+    else if (path.toLocaleLowerCase() === '/about') {
+        response.end(html.replace('{{%content%}}', 'You are in About Page'))
+    }
+    else if (path.toLocaleLowerCase() === '/contact') {
+        response.end(html.replace('{{%content%}}', 'You are in Contact Page'))
+    }
+    else
+        response.end(html.replace('{{%content%}}', 'Error 404: Page Not Found'))
 });
 
 
